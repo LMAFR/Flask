a3139c1cd2b3b199ac5e2e10159bdb611b387f76
Alejandro Florido KPMG <alejandrofloridoreyes@gmail.com>
2023-01-01 18:48:36 +0100

    Now the text in the page changes dynamically with the URL text. In addition, HTML escaping has been implemented manually in order to avoid HTML injections.

--- Documents/Flask/flask_quickstart/app.py
+++ Documents/Flask/flask_quickstart/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "<h1>Hello World!</h1>"
+@app.route('/<name>')
+def hello_world(name):
+    return f"Hello {escape(name)}!"
