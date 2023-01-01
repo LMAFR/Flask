fc90b82116eb925eab899b12163088cedb2102f1
Alejandro Florido KPMG <alejandrofloridoreyes@gmail.com>
2023-01-01 19:07:57 +0100

    Testing some ways of change how our pages looks by using dynamic routes.

--- Documents/Flask/flask_quickstart/app.py
+++ Documents/Flask/flask_quickstart/app.py
@@ -10,3 +10,18 @@ def index():
 @app.route('/<name>')
 def hello_world(name):
     return f"Hello {escape(name)}!"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # Show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # Show the id of the current post
+    return f'Post {escape(post_id)}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # Show the subpath after /path/
+    return f'Subpath: {escape(subpath)}'
