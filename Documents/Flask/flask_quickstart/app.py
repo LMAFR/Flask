1a5b54f80e40175acf9d4ffa2ba45a01294f2d57
Alejandro Florido KPMG <alejandrofloridoreyes@gmail.com>
2023-01-01 18:56:03 +0100

    New route added.

--- Documents/Flask/flask_quickstart/app.py
+++ Documents/Flask/flask_quickstart/app.py
@@ -3,6 +3,10 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'Index Page'
+
 @app.route('/<name>')
 def hello_world(name):
     return f"Hello {escape(name)}!"
