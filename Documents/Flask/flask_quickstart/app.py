4d35e6e1d50ce08002dbb303b2e844b4cb0c5452
Alejandro Florido KPMG <alejandrofloridoreyes@gmail.com>
2023-01-04 15:54:02 +0100

    testing request object.

--- Documents/Flask/flask_quickstart/app.py
+++ Documents/Flask/flask_quickstart/app.py
@@ -39,13 +39,25 @@ def projects():
 def about():
     return 'The about page'
 
-@app.route('/login', methods=['GET', 'POST'])
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         return do_the_login()
+#     # By default, the method is POST
+#     else:
+#         return show_the_login_form()
+
+# request object
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    error = None
     if request.method == 'POST':
-        return do_the_login()
-    # By default, the method is POST
-    else:
-        return show_the_login_form()
+        if valid_login(request.form['username'],
+                       request.form['password']):
+           return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    return render_template('login.html', error = error)
 
 with app.test_request_context():
     print(url_for('index'))
@@ -60,5 +72,7 @@ with app.test_request_context():
 with app.test_request_context('/hello', method = 'POST'):
     assert request.path == '/hello'
     assert request.method == 'POST'
+
+
 
 
