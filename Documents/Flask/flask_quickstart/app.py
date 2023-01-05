8a27da43a45b9b1b186850ea7573fb79afb37228
Alejandro Florido KPMG <alejandrofloridoreyes@gmail.com>
2023-01-05 18:33:55 +0100

    Learning how to build JSONs in order to extract/insert data through APIs.

--- Documents/Flask/flask_quickstart/app.py
+++ Documents/Flask/flask_quickstart/app.py
@@ -91,6 +91,19 @@ def upload_file():
         f = method.files['test.txt']
         f.save(f'/var/www/uploads/{secure_filename(f.filename)}')
 
+@app.route('/me')
+def me_api():
+    user = get_current_user()
+    return {
+        'username':user.username,
+        'theme':user.theme,
+        'image':url_for('user_image', filename=user.image),
+    }
+
+@app.route('/users')
+def users_api():
+    users = get_all_users()
+    return [user.to_json() for user in users]
 
 
 
