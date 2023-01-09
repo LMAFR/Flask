ff12e729e5403eb9758ec196fe201b70803887bf
Alejandro Florido KPMG <alejandrofloridoreyes@gmail.com>
2023-01-09 11:48:31 +0100

    Create New Post feature added.

--- Documents/Flask/doc_tutorial/flaskr/blog.py
+++ Documents/Flask/doc_tutorial/flaskr/blog.py
@@ -18,3 +18,29 @@ def index():
     
     # In the line below I think posts is an optional argument, being the argument variable the one we will use in the HTML template.
     return render_template('blog/index.html', posts=posts)
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required'
+        if error is not None:
+            flash(error)
+        
+        else:
+            db =get_db()
+            db.execute(
+                "INSERT INTO post (title, body, author_id)"
+                "VALUES (?,?,?)",
+                (title, body, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/create.html')
+
