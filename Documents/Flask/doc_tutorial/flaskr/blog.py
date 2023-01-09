a1ac1daa0ef2f0717cfdf66469ed09de1a998759
Alejandro Florido KPMG <alejandrofloridoreyes@gmail.com>
2023-01-09 11:12:15 +0100

    Index page (associated with blog blueprint) has been created.

--- Documents/Flask/doc_tutorial/flaskr/blog.py
+++ Documents/Flask/doc_tutorial/flaskr/blog.py
@@ -6,3 +6,15 @@ from flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        "SELECT p.id, title, body, created, author_id, username"
+        " FROM post p JOIN user u WHERE p.author_id = u.id"
+        " ORDER BY created DESC"
+    ).fetchall()
+    
+    # In the line below I think posts is an optional argument, being the argument variable the one we will use in the HTML template.
+    return render_template('blog/index.html', posts=posts)
