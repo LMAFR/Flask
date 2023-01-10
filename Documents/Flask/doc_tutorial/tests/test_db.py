deae4616906bc1a4f661cae79887f86b9fa86bb1
Alejandro Florido KPMG <alejandrofloridoreyes@gmail.com>
2023-01-10 11:51:25 +0100

    Tests for the database have been added.

---
+++ Documents/Flask/doc_tutorial/tests/test_db.py
@@ -0,0 +1,27 @@
+import sqlite3
+import pytest
+from flaskr.db import get_db
+
+def test_get_close_db(app):
+    with app.appcontext():
+        db = get_db()
+        assert db is get_db()
+
+    with pytest.raises(sqlite3.ProgrammingError) as e:
+        db.execute('SELECT 1')
+    
+    assert 'closed' in str(e.value)
+
+def test_init_db_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
+    
+    def fake_inib_db():
+        Recorder.called = True
+
+    # monkeypatch replaces init_db by a function (fake_init_db) that records that it has been called.
+    monkeypatch.setattr('flaskr.db.init_db', fake_inib_db)
+    # result is the result of calling init_db by using command line.
+    result = runner.invoke(args=['init-db'])
+    assert 'Initialized' in result.output
+    assert Recorder.called
