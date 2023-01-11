f97076ec4fa0909ba81c2ac545d93a5c2d22c853
Alejandro Florido KPMG <alejandrofloridoreyes@gmail.com>
2023-01-11 10:23:23 +0100

    Blog tests for wrong users trying to access update or delete postv have been written.

--- Documents/Flask/doc_tutorial/tests/test_blog.py
+++ Documents/Flask/doc_tutorial/tests/test_blog.py
@@ -14,4 +14,35 @@ def test_index(auth, client):
     assert b'test \nbody' in response.data
     assert b'href="/1/update/"' in response.data
 
+@pytest.mark.parametrize('path', (
+    '/create',
+    '/1/update',
+    '/1/delete',
+))
+def test_login_required(client, path):
+    response = client.post(path)
+    assert response.headers['Location'] == '/auth/login'
 
+def test_author_required(client, app, auth):
+    # Change the post author to another user
+    with app.app_context():
+        db = get_db()
+        db.execute(
+            "UPDATE post SET author_id = 2 WHERE id = 1"
+        )
+        db.commit()
+
+    auth.login()
+    # If the user is not the author of the post, he should not be able to update/delete it
+    assert client.post('/1/update').status_code == 403
+    assert client.post('/1/delete').status_code == 403
+    # If the user is not the author of the post, he should not be able to see the edit button.
+    assert b'href="/1/update"' not in client.get('/').data
+
+@pytest.mark.parametrize('path', (
+    '/2/update',
+    '/2/delete',
+))
+def test_exists_required(auth, client):
+    auth.login()
+    assert client.post(path).status_code == 404
